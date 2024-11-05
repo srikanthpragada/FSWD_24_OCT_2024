92f8d3d76ca470bea8bb79f935bd18f6bd066206
srikanthpragada <srikanthpragada@gmail.com>
2024-11-05 20:56:23 +0530

    05-NOV

--- js/oop/counter.js
+++ js/oop/counter.js
@@ -1,10 +1,30 @@
 class Counter {
+     constructor(value) {
+        this.start = value 
+        this.value = value 
+     }
 
+     increment() {
+        this.value ++
+     }
+
+     decrement() {
+        this.value --
+     }
+
+     getValue() {
+         return this.value
+     }
+
+     reset() {
+        this.value = this.start 
+     }
 }
 
 let c1 = new Counter(100)
 c1.increment()
 console.log(c1.getValue())  // 101 
+console.log(c1.value)
 c1.reset() 
 console.log(c1.getValue())  // 100 
 
